f7a812f30d231468827620ca3b97b949851276bb
Alexander Stagun <alex@ascii-park.net>
2011-11-04 19:11:34 +0100

    added test for problem-16

--- test/clojure_euler/test/problems.clj
+++ test/clojure_euler/test/problems.clj
@@ -1,5 +1,6 @@
 (ns clojure-euler.test.problems
   (:use [clojure-euler.problem-1])
+  (:use [clojure-euler.problem-16])
   (:use [clojure-euler.problem-20])
   (:use [clojure-euler.problem-52])
   (:use [clojure.test]))
@@ -10,6 +11,9 @@
 
 (deftest test-problem-20
   (is (= 27 (problem-20 10))))
+
+(deftest test-problem-16
+  (is (= 26 (problem-16 15))))
 
 (deftest test-problem-52
   (is (= 125874 (problem-52 '(1 2)))))
